44d2cc11066afe6db1cae6c583da3fd614cb8724
Cleber de Brito <trisken@gmail.com>
2015-01-07 08:22:48 -0200

    Arquivos inseridos novamente

---
+++ ALS_CORPORATIVO/Scripts/Erro.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+
+
+
+});
+/////////////////////////////////////////////////////////////////////////////////////////////
+function Redireciona(tipo) {
+
+    var destino = "../Home/Home.aspx";
+
+    if (tipo == 1) {
+        destino = "../Login/Login.aspx";
+    }
+
+    window.location.href = destino;
+}
